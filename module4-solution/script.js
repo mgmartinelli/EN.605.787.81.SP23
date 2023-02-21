40cf33600c1ff50366b8cb422b0827c63b754576
Mariela Guardado-Martinelli <mgmartinelli1@gmail.com>
2023-02-20 20:08:15 -0500

    added functionality using Array.prototype.map

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -29,11 +29,6 @@ WARNING!!! WARNING!!!
 
 */
 
-// STEP 1:
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
-
 var names = [
   "Yaakov",
   "John",
@@ -48,29 +43,26 @@ var names = [
 ];
 
 (function (names) {
-  // STEP 10:
-  // Loop over the names array and say either 'Hello' or "Good Bye"
-  // using either the helloSpeaker's or byeSpeaker's 'speak' method.
-  // See Lecture 50, part 1
   for (var i = 0; i < names.length; i++) {
-    // STEP 11:
-    // Retrieve the first letter of the current name in the loop.
-    // Use the string object's 'charAt' function. Since we are looking for
-    // names that start with either upper case or lower case 'J'/'j', call
-    // string object's 'toLowerCase' method on the result so we can compare
-    // to lower case character 'j' afterwards.
-    // Look up these methods on Mozilla Developer Network web site if needed.
     var firstLetter = names[i].charAt(0).toLowerCase();
 
-    // STEP 12:
-    // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-    // name in the loop.
     if (firstLetter === "j") {
       byeSpeaker.speak(names[i]);
     } else {
       helloSpeaker.speak(names[i]);
     }
   }
+
+  function generateGreeting(name) {
+    var firstLetter = name.charAt(0).toLowerCase();
+
+    if (firstLetter === "j") {
+      return byeSpeaker.speakSimple(name);
+    } else {
+      return helloSpeaker.speakSimple(name);
+    }
+  }
+
+  const greetings = names.map(generateGreeting);
+  greetings.map((greeting) => console.log(greeting));
 })(names);
