6f40e102b752be2cf892b0e6b61fb9f7e5609d8f
Mariela Guardado-Martinelli <mgmartinelli1@gmail.com>
2023-03-07 20:16:08 -0500

    Implemented baseline mod6 solution

---
+++ module6-solution/js/app.js
@@ -0,0 +1,28 @@
+(function () {
+  angular
+    .module("LunchCheck", [])
+    .controller("LunchCheckController", LunchCheckController);
+
+  LunchCheckController.$inject = ["$scope"];
+  function LunchCheckController($scope) {
+    $scope.validationMessage = "";
+
+    $scope.checkMenu = function () {
+      const input = $scope.menu;
+
+      if (input !== undefined) {
+        const inputSplit = input.split(",");
+
+        if (input === "" || inputSplit.length < 1) {
+          $scope.validationMessage = "Please enter data first";
+        } else if (inputSplit.length <= 3) {
+          $scope.validationMessage = "Enjoy!";
+        } else {
+          $scope.validationMessage = "Too much!";
+        }
+      } else {
+        $scope.validationMessage = "Please enter data first";
+      }
+    };
+  }
+})();
